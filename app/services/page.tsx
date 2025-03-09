0a9f8d9b950019d8a45f15b289692a15e3a3faf8
uwamba <uwambadodo@gmail.com>
2025-03-09 15:52:01 +0200

    installaion and setup main features

---
+++ app/services/page.tsx
@@ -0,0 +1,27 @@
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import Breadcrumb from "@/components/Common/Breadcrumb";
+import Contact from "@/components/Contact";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Page | Free Next.js Template for Startup and SaaS",
+  description: "This is About Page for Startup Nextjs Template",
+  // other metadata
+};
+
+const AboutPage = () => {
+  return (
+    <>
+      <Breadcrumb
+        pageName="About Page"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+      />
+      <AboutSectionOne />
+      <Contact />
+    </>
+  );
+};
+
+export default AboutPage;

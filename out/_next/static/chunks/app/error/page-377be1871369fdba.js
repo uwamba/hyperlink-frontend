12bf25c55fe6b21591268e1f77279db34646a342
gapetitheos@gmail.com <uwambadodo@gmail.com>
2025-10-11 13:31:50 +0200

    new chnage

---
+++ out/_next/static/chunks/app/error/page-377be1871369fdba.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[879,9527],{95553:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6874,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[6874,8441,1684,7358],()=>s(95553)),_N_E=e.O()}]);
